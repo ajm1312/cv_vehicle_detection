80249260a5451f9bc6ba337d2af411ddfcae9382
Andrew <andrew.mitchellcs@gmail.com>
2025-11-26 00:45:58 -0500

    updated model settings

--- classifier/train.py
+++ classifier/train.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 model = YOLO('yolov8s.pt')
 
 results = model.train(
-    data='data.yaml',   # This file tells YOLO where your folders are
-    epochs=50,          # How many times to cycle through the data
-    imgsz=640,          # Image resolution
-    batch=16,           # How many images to process at once
-    name='vehicle_custom_model' # Saves results to runs/detect/vehicle_custom_model
+    data='data.yaml',
+    epochs=50,
+    imgsz=640,
+    batch=16,
+    name='vehicle_custom_model'
 )
